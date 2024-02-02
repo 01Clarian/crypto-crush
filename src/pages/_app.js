fcd37aac5161ed2fde7e749c1517865d8c8dcee2
01clarian <clarionnorth@gmail.com>
2024-02-02 11:13:14 -0600

    transpiler added for redux, module reconfigs for title and index recongif change from home page for a cleaner route

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,31 +1,27 @@
 import { Provider } from 'react-redux';
-import { store } from '../store'; // Adjust the path to your store
+import { store } from '../store';
 import '../styles/globals.css';
-import '../styles/title.css';
+import '../styles/title.module.css';
 import dynamic from 'next/dynamic';
-
-import Head from 'next/head';
 import Script from 'next/script';
 
-const DynamicPage = dynamic(() => import('./home.module'), {
-    ssr: false, // Disable SSR for this component
+const DynamicPage = dynamic(() => import('./index'), {
+  ssr: false, // Disable SSR for this component
 });
 
-const MyApp = ({ Component, pageProps }) => {
-    return (
-        <div>
-            <Script
-                src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r134/three.min.js"
-                strategy="beforeInteractive"
-            />
-            <Script src="https://cdn.jsdelivr.net/npm/vanta/dist/vanta.waves.min.js"
-            ></Script>
-            <Provider store={store}>
-                <Component {...pageProps} />
-                <DynamicPage {...pageProps} /> {/* Render the dynamic page */}
-            </Provider>
-        </div>
-    );
+const MyApp = ({ pageProps }) => { // Note: Component prop is removed
+  return (
+    <div>
+      <Script
+        src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r134/three.min.js"
+        strategy="beforeInteractive"
+      />
+      <Script src="https://cdn.jsdelivr.net/npm/vanta/dist/vanta.waves.min.js" />
+      <Provider store={store}>
+        <DynamicPage {...pageProps} /> {/* Render the dynamic page directly */}
+      </Provider>
+    </div>
+  );
 };
 
-export default MyApp;
+export default MyApp; // Export DynamicPage as the default
